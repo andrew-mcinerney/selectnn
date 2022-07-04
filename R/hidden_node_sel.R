f305ebfb239304affe5cf974d76bda4251e395c9
Andrew McInerney <andrew.mcinerney@ul.ie>
2022-07-04 10:16:57 +0100

    styler hidden_node_sel

--- R/hidden_node_sel.R
+++ R/hidden_node_sel.R
@@ -12,52 +12,44 @@
 #' @param ... additional argument for nnet
 #' @return Optimal number of hidden nodes
 #' @export
-hidden_node_sel = function(X, y, Q, n_init, type = 'bulk', inf_crit = 'BIC', unif = 3, ...){
-
-  if (type == 'bulk') {
-
+hidden_node_sel <- function(X, y, Q, n_init, type = "bulk", inf_crit = "BIC", unif = 3, ...) {
+  if (type == "bulk") {
     inf_crit_vec <- rep(NA, Q)
 
     names(inf_crit_vec) <- as.character(1:Q)
 
-    weights_min <- vector('list', length = Q)
+    weights_min <- vector("list", length = Q)
 
-    for(q in 1:Q){
-
+    for (q in 1:Q) {
       nn <- nn_fit_tracks(X, y, q, n_init, inf_crit, unif, ...)
 
       weights_min[[q]] <- nn$W_opt
 
       inf_crit_vec[q] <- nn$val
-
     }
 
     W_opt <- weights_min[[which.min(inf_crit_vec)]]
-
-  } else if (type == 'step') {
-
+  } else if (type == "step") {
     n_candidates <- 3
 
     inf_crit_vec <- rep(NA, n_candidates)
 
     names(inf_crit_vec) <- as.character((Q - 1):(Q + 1))
 
-    n = nrow(X) # sample size
-    p = ncol(X) # number of covariates
+    n <- nrow(X) # sample size
+    p <- ncol(X) # number of covariates
 
-    weights_min = vector(mode = "list", length = n_candidates)
+    weights_min <- vector(mode = "list", length = n_candidates)
     names(weights_min) <- as.character((Q - 1):(Q + 1))
 
 
     for (q in (Q - 1):(Q + 1)) {
-
-      if (q == 0){
+      if (q == 0) {
         weights_min[[as.character(q)]] <- NULL
 
         inf_crit_vec[as.character(q)] <- NA
       } else {
-
-        k = (p + 1)*q + (q + 1)
+        k <- (p + 1) * q + (q + 1)
 
 
         nn <- nn_fit_tracks(X, y, q, n_init, inf_crit, unif, ...)
@@ -65,18 +57,17 @@ hidden_node_sel = function(X, y, Q, n_init, type = 'bulk', inf_crit = 'BIC', uni
         weights_min[[as.character(q)]] <- nn$W_opt
 
         inf_crit_vec[as.character(q)] <- nn$val
-
       }
-
     }
 
     W_opt <- weights_min[[names(which.min(inf_crit_vec))]]
-
   }
 
-  return(list('min' = as.numeric(names(which.min(inf_crit_vec))),
-              'val' = min(inf_crit_vec, na.rm = TRUE),
-              'inf_crit_vec' = inf_crit_vec,
-              'weights_min' = weights_min,
-              'W_opt' = W_opt))
+  return(list(
+    "min" = as.numeric(names(which.min(inf_crit_vec))),
+    "val" = min(inf_crit_vec, na.rm = TRUE),
+    "inf_crit_vec" = inf_crit_vec,
+    "weights_min" = weights_min,
+    "W_opt" = W_opt
+  ))
 }
